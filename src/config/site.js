c649d98f3a309be2d3c50b0ac1be0a289f1b812a
Felipe <adm.otmizards@gmail.com>
2026-01-03 12:33:36 +0000

    fix: update SITE.url to site2 domain

--- src/config/site.js
+++ src/config/site.js
@@ -1,6 +1,7 @@
 export const SITE = {
   name: "Casino Online para Iniciantes",
-  description: "Guia para iniciantes em casino online em Portugal: primeiros passos, regras e boas práticas.",
-  url: "https://site02.360winspt.com",
+  description:
+    "Guia para iniciantes em casino online em Portugal: primeiros passos, regras e boas práticas.",
+  url: "https://site2.360winspt.com",
   telegram: "https://t.me/+cIQNof1Xess1NDE8",
 };
